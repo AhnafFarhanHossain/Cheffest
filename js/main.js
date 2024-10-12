884e22bbff9c4231d86c553e9d53dcf0ca5a5ea1
Ahnaf Farhan Hossain <161995795+AhnafFarhanHossain@users.noreply.github.com>
2024-10-12 10:23:09 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("nav").style.display = "block";
+    document.getElementById("firstLogo").style.display = "none"
+    document.getElementById("logo").style.display = "none"
+}
+function closeNav() {
+    document.getElementById("nav").style.display = "none"
+    document.getElementById("firstLogo").style.display = "block"
+}
